6a70b9fcf3e2b4d81c82574e60993474060f6ac6
Mark Wild <mark.wild189@gmail.com>
2020-03-18 13:54:10 -0700

    commit

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,6 +2,7 @@ import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 //Initial State
 const initialState = {
+  //dummy transactions
   //expense negative number, income positive number
   transactions: [
     { id: 1, text: 'Flower', amount: -20 },
@@ -17,11 +18,12 @@ export const GlobalContext = createContext(initialState);
 
 // Provider component
 // props are named children because we are wraping components in App
+//children = everything globalprovider is wrapped around
 export const GlobalProvider = ({ children }) => {
   //use reducer usereducer takes in wherever are reducer is => appreducer file
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
-  //provider componentn provides state and actions to the components that its wrapped around
+  //provider component provides state and actions to the components that its wrapped around
   return (
     <GlobalContext.Provider
       value={{
